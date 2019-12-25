df3442c635a7bba12e4a1bc75d23cde695ea8375
supermanc88 <supermanc88@gmail.com>
2019-12-25 18:43:41 +0800

    添加附加到gui线程代码

--- GetActiveWindowUser/main.cpp
+++ GetActiveWindowUser/main.cpp
@@ -1,23 +1,38 @@
 #include <Windows.h>
+#include <stdio.h>
 
 int main()
 {
 
-	HANDLE handle = CreateFileW(L"\\\\.\\ActiveWindow",
-		GENERIC_READ | GENERIC_READ,
-		FILE_SHARE_READ | FILE_SHARE_WRITE,
-		NULL,
-		OPEN_EXISTING,
-		FILE_ATTRIBUTE_NORMAL,
-		NULL);
+	// HANDLE handle = CreateFileW(L"\\\\.\\ActiveWindow",
+	// 	GENERIC_READ | GENERIC_READ,
+	// 	FILE_SHARE_READ | FILE_SHARE_WRITE,
+	// 	NULL,
+	// 	OPEN_EXISTING,
+	// 	FILE_ATTRIBUTE_NORMAL,
+	// 	NULL);
+	//
+	//
+	// char lpBuffer[20] = { 0 };
+	// DWORD numberOfBytesToRead = 0;
+	// DWORD num = 0;
+	// ReadFile(handle, lpBuffer, numberOfBytesToRead, &num, NULL);
+	//
+	// CloseHandle(handle);
 
 
-	char lpBuffer[20] = { 0 };
-	DWORD numberOfBytesToRead = 0;
-	DWORD num = 0;
-	ReadFile(handle, lpBuffer, numberOfBytesToRead, &num, NULL);
+	while (TRUE)
+	{
+		HWND hwnd = GetForegroundWindow();
 
-	CloseHandle(handle);
+		HWND hwnd1 = GetActiveWindow();
+
+		printf("GetForegroundWindow hwnd %p, GetActiveWindow hwnd1 %p\n", hwnd, hwnd1);
+
+		Sleep(1000);
+	}
+	
+
 
 	return 0;
 }
